426ef041e48d9388016005a776b6128912f488c7
baclan228 <dimaisasha52@gmail.com>
2019-01-16 01:51:27 +0300

    v 1.4

--- www/block/header.php
+++ www/block/header.php
@@ -1,11 +1,15 @@
 ﻿<header class="header" class="close">
+	<div class='logo'>
+		<a href='index.php'>Bonus.com</a>
+	</div>
+	<div class='raz'>
 		<ul>
 			<li>
-				<span><b>Bonus.ru</b></span>
 				<a href="index.php">Бонус</a>
 				<a href="box.php">Сундуки</a>
 				<a href="payments.php">Выплаты</a>
 				<a href="account.php">Профиль</a>
 			</li>
 		</ul>
+	</div>
 	</header>
